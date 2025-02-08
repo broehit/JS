15986dc92c4857b0b2f1cd3dc1dd6686f9f8cba3
Rohit Manal <153975726+broehit@users.noreply.github.com>
2025-02-08 15:14:16 +0000

    datatypes

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,10 @@
+"use strict"; //to apply new version of js
+
+alert(6+9) // since we are working in node js alert wont work here but will work in browser
+
+
+let  name="Rohit"
+let age=53
+let lie=true
+null // standalone empty value
+
