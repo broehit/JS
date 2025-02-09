0c8d111d147fbb8e5898e2481088accd1f93b649
Rohit Manal <153975726+broehit@users.noreply.github.com>
2025-02-09 08:37:34 +0000

    conversionoperation

---
+++ 01_basics/03_conversionOprt.js
@@ -0,0 +1,22 @@
+let score="23abhc"
+
+console.log(typeof (score));
+
+let valueInNum = Number(score)
+console.log (typeof valueInNum )
+console.log(valueInNum)
+
+/* "23"= 23
+   "23abhc"= NaN
+   true=1, false=0 */
+
+   let LogedIn= 0
+
+   let booleanValue= Boolean(LogedIn)
+   console.log(booleanValue)
+     
+   console.log(typeof (booleanValue))
+   /* 1= true
+   09= false
+   ''=false
+   'rohit'= true */
