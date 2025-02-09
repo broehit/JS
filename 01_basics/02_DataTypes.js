0c4b79286a40ccb2331e61196297e48ad9ef131e
Rohit Manal <153975726+broehit@users.noreply.github.com>
2025-02-09 08:00:21 +0000

    datatypes

--- 01_basics/02_DataTypes.js
+++ 01_basics/02_DataTypes.js
@@ -6,5 +6,4 @@ alert(6+9) // since we are working in node js alert wont work here but will work
 let  name="Rohit"
 let age=53
 let lie=true
-null // standalone empty value
-
+null // standalone value that 
